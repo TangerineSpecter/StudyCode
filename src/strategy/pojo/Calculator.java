ca6a8daa29125f4cbd409f388f28c47017cb44a0
TangerineSpecter <993033472@qq.com>
2019-02-11 21:59:11 +0800

    策略模式学习

---
+++ src/strategy/pojo/Calculator.java
@@ -0,0 +1,16 @@
+package strategy.pojo;
+
+import strategy.service.Operation;
+
+public class Calculator {
+
+	private Operation operation;
+
+	public void setOperation(Operation operation) {
+		this.operation = operation;
+	}
+
+	public int doOperation(int num1, int num2) {
+		return this.operation.doOperation(num1, num2);
+	}
+}
---
+++ src/strategy/service/Operation.java
@@ -0,0 +1,6 @@
+package strategy.service;
+
+public interface Operation {
+
+	public int doOperation(int num1, int num2);
+}
