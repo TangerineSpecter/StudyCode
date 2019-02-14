12b766a438681c9cf9b1b92ccb6526fff540986b
TangerineSpecter <993033472@qq.com>
2019-02-14 11:33:36 +0800

    单例模式

---
+++ src/singleton/SingleObjectEager.java
@@ -0,0 +1,31 @@
+package singleton;
+
+/**
+ * 单例模式--饿汉式
+ * 
+ * @author TangerineSpecter
+ * @Datetime 2019年2月14日
+ *
+ */
+public class SingleObjectEager {
+
+	private static SingleObjectEager instance = new SingleObjectEager();
+
+	public SingleObjectEager() {
+	};
+
+	/**
+	 * 饿汉式
+	 * 
+	 * 在类加载时候就创建好对象实例，保证了实例的唯一性。
+	 * 
+	 * 优点：对象在内存中只有一个实例，并且无需频繁的创建对象和销毁对象，大大减少了性能损耗。由于一开始就初始化实例，所以获取对象速度很快。
+	 * 
+	 * 缺点：一开始就进行初始化实例，不管使不使用都会使用一部分内存。
+	 * 
+	 * 
+	 */
+	public static SingleObjectEager getInstance() {
+		return instance;
+	}
+}
---
+++ src/singleton/SingleObjectStatic.java
@@ -0,0 +1,26 @@
+package singleton;
+
+/**
+ * 单例模式--静态内部类方式
+ * 
+ * @author TangerineSpecter
+ * @Datetime 2019年2月14日
+ *
+ */
+public class SingleObjectStatic {
+
+	private static class Singleton {
+		private static SingleObjectStatic single = new SingleObjectStatic();
+	}
+
+	/**
+	 * 静态内部类Singleton在初始化的时候不会被加载，只有get的时候才会实例化。也算懒汉式的一种。
+	 * 
+	 * 而静态内部类只会被实例化一次，不管多少的线程并发，大家拿到的都是同一个实例，线程是安全的。
+	 * 
+	 * 没有使用synchronized进行加锁，所以并发性高，线程安全，性能很好。
+	 */
+	public static SingleObjectStatic getInstance() {
+		return Singleton.single;
+	}
+}
