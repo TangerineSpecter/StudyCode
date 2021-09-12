663166e784273d8e0f9c4123ba966e2f86c742d1
TangerineSpecter <993033472@qq.com>
2021-09-12 19:33:42 +0800

    test3

--- src/main/java/com/tangerine/specter/StudyApplication.java
+++ src/main/java/com/tangerine/specter/StudyApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudyApplication {
 
     public static void main(String[] args) {
-        System.out.println("测试代码rebase test1");
+        System.out.println("测试代码rebase test3");
         SpringApplication.run(StudyApplication.class, args);
     }
 
